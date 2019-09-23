4d2bbe514979bd9654fcd6bdca68aac6ea43e147
mohadesegharakhanlo <47597127+mohadesegharakhanlo@users.noreply.github.com>
2019-09-23 13:25:06 +0330

    struct

--- struct.cpp
+++ struct.cpp
@@ -9,14 +9,45 @@ struct ali{
     string fname;
     string lname;
 };
-
+struct student{
+    string name;
+    int age;
+    
+};
+struct pointer
+{
+    int x;
+    int y;
+    int *z;
+};
 
 int main()
-{    cin>>p1.fname;
+{  /* cout<<"enter p1.fname:"; 
+    cin>>p1.fname;
     cout<<p1.fname<<endl;
     struct ali p2;
+    cout<<"enter p2.fname:";
     cin>>p2.fname;
     cout<<p2.fname<<endl;
-    cout<<p1.fname<<" LOVES "<<p2.fname;
+    cout<<p1.fname<<" LOVES "<<p2.fname<<endl;*/ //ali and mohadese
+
+   /* struct student info[2];
+    for(int i=0;i<2;i++){
+       cout<<"enter your name:";
+       cin>>info[i].name;
+       cout<<"enter your age:";
+       cin>>info[i].age;
+    }
+    for(int i=0;i<2;i++)
+    {
+        cout<<info[i].name<<endl <<info[i].age<<endl;
+        
+    }*/ //students
+    int zz=7;
+    pointer a;
+    pointer*b;
+    b=&a;
+    
+
     return 0;
 }
